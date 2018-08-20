4be9151ffa560accf0496eedeca88d10ef56fdb8
Danny <dannyxmcclain@gmail.com>
2018-08-20 14:41:51 -0500

    addin an input

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value + ' was submitted');
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input className="add-entry" type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Add" />
+      </form>
       </div>
     );
   }
