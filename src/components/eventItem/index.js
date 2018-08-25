707e15bf20ac33d899f542415498875168e63d71
Danny <dannyxmcclain@gmail.com>
2018-08-25 07:17:28 -0500

    added emojimart!

--- src/components/eventItem/index.js
+++ src/components/eventItem/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
 import DeleteButton from './../deleteButton';
 
 const EventItem = ({name, date, daysUntil, handleDelete}) => (
   <div className="event-item">
-    <div className="event-left">
-      <p className="event-name">{name}</p>
-      <p className="event-date">{date}</p>
-    </div>
-    <div className="event-right">
-      <p className="days-until">{daysUntil}</p>
-      <DeleteButton
-        onDelete={handleDelete}
-      />
-    </div>
+    <Picker
+      title=''
+      showPreview={false}
+      style={{ 
+        position: 'absolute', 
+        top: '180px', 
+        left: '10px',
+        opacity: '0.5',
+        transform: 'translateY(-10%)',
+      }}
+      color='#0099FF'
+      perLine='7'
+    />
+    <p className="event-name">{name}</p>
+    <p className="event-date">{date}</p>
+    <p className="days-until">{daysUntil}</p>
+    <DeleteButton
+      onDelete={handleDelete}
+    />
   </div>
 )
 
