46e7cf04a56d33e57bc14aa916f4f176e5f0114d
Danny <dannyxmcclain@gmail.com>
2018-09-19 11:04:16 -0500

    editing layout a bit

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ import './App.css';
 
 class App extends Component {
   state = {
-      myDates: {},
-      showForm: false,
-      currentEvent: null
-  }
+    myDates: {},
+    showForm: false,
+    currentEvent: null
+  };
 
   componentDidMount() {
-    const localState = localStorage.getItem("state");
-    if (localState){
+    const localState = localStorage.getItem('state');
+    if (localState) {
       this.setState({
         myDates: JSON.parse(localState)
-      })
+      });
     }
 
-    document.addEventListener('keydown', (event) => {
+    document.addEventListener('keydown', event => {
       if (event.code === 'Escape') {
         this.setState({
           showForm: false
-        })
+        });
       }
     });
   }
@@ -37,10 +37,10 @@ class App extends Component {
   }
 
   componentDidUpdate() {
-    localStorage.setItem("state", JSON.stringify(this.state.myDates));
+    localStorage.setItem('state', JSON.stringify(this.state.myDates));
   }
 
-  handleSubmit = (myEvent) => {
+  handleSubmit = myEvent => {
     this.setState({
       myDates: {
         ...this.state.myDates,
@@ -49,45 +49,48 @@ class App extends Component {
       showForm: false,
       currentEvent: null
     });
-  }
+  };
 
   toggleEditing = () => {
-    this.setState( (currentState) => {
-      return {
-        showForm: !currentState.showForm,
-        currentEvent: null
+    this.setState(
+      currentState => {
+        return {
+          showForm: !currentState.showForm,
+          currentEvent: null
+        };
+      },
+      () => {
+        this.state.showForm && this.nameInput.focus();
       }
-    }, () => {
-      this.state.showForm && this.nameInput.focus()
-    });
-  }
+    );
+  };
 
-  bindInnerRef = (htmlNode) => {
+  bindInnerRef = htmlNode => {
     this.nameInput = htmlNode;
-  }
+  };
 
-  removeItem = ({id}) => {
-    const datesCopy = {...this.state.myDates};
+  removeItem = ({ id }) => {
+    const datesCopy = { ...this.state.myDates };
     delete datesCopy[id];
-    this.setState ({
+    this.setState({
       myDates: datesCopy
-    })
-  }
+    });
+  };
 
-  handleEdit = (event) => {
+  handleEdit = event => {
     this.setState({
       currentEvent: event,
       showForm: true
     });
-  }
+  };
 
-  eventFormatter = (event) => {
+  eventFormatter = event => {
     let eventOverrides = {};
     let eventDate = moment(event.date, 'MMM D, YYYY');
     let today = moment();
     if (eventDate.isBefore(today, 'day')) {
       eventOverrides.status = 'past';
-    } else if (eventDate.isSame(today, 'day')){
+    } else if (eventDate.isSame(today, 'day')) {
       eventOverrides.status = 'present';
       eventOverrides.daysUntil = 'Today!';
     } else {
@@ -95,71 +98,65 @@ class App extends Component {
     }
 
     return {
-      ...event, 
+      ...event,
       ...eventOverrides
-    }
-  }
+    };
+  };
 
   eventReorder = (event1, event2) => {
-    return moment.utc(event1.date, "MMM D, YYYY").diff(moment.utc(event2.date, "MMM D, YYYY"))
-  }
-  
+    return moment
+      .utc(event1.date, 'MMM D, YYYY')
+      .diff(moment.utc(event2.date, 'MMM D, YYYY'));
+  };
 
   render() {
     return (
       <div>
-        <Modal
-          isShowing={this.state.showForm}
-          handleClose={this.toggleEditing} >
-            <CardForm 
+        <Modal isShowing={this.state.showForm} handleClose={this.toggleEditing}>
+          <CardForm
             innerRef={this.bindInnerRef}
-            handleSubmit={this.handleSubmit} 
+            handleSubmit={this.handleSubmit}
             isShowing={this.state.showForm}
             currentEvent={this.state.currentEvent}
-            />
-        </Modal>
-          
-        <header>
-          <div className="container">
-            <h1>Until</h1>
-            <ActionButton 
-              handleClick={this.toggleEditing} 
-              isActive={this.state.showForm}
-            />
-          </div>
-        </header>
-        { Object.keys(this.state.myDates).length > 0 ? 
-          <div className="event-list">
-          <ReactCSSTransitionGroup
-          transitionName="cardAnimate"
-          transitionEnterTimeout={350}
-          transitionLeaveTimeout={200}>
-          {Object.values(this.state.myDates).map(this.eventFormatter).sort(this.eventReorder).map((event) => (
-            <EventItem
-              key={event.id}
-              {...event}
-              handleDelete={() => this.removeItem(event)}
-              handleEdit={() => this.handleEdit(event)}
-              eventStatus={event.staus}
-            />
-          ))}
-          <AddCard handleClick={this.toggleEditing}
-            isActive={this.state.showForm}
           />
-          </ReactCSSTransitionGroup>
+        </Modal>
+
+        {Object.keys(this.state.myDates).length > 0 ? (
+          <div className="event-list">
+            <ReactCSSTransitionGroup
+              transitionName="cardAnimate"
+              transitionEnterTimeout={350}
+              transitionLeaveTimeout={200}
+            >
+              {Object.values(this.state.myDates)
+                .map(this.eventFormatter)
+                .sort(this.eventReorder)
+                .map(event => (
+                  <EventItem
+                    key={event.id}
+                    {...event}
+                    handleDelete={() => this.removeItem(event)}
+                    handleEdit={() => this.handleEdit(event)}
+                    eventStatus={event.staus}
+                  />
+                ))}
+              <AddCard
+                handleClick={this.toggleEditing}
+                isActive={this.state.showForm}
+              />
+            </ReactCSSTransitionGroup>
           </div>
-          :  
+        ) : (
           <div className="event-list no-events">
             <span>
-            <h1>Add one ya dummy.</h1>
-            <AddCard handleClick={this.toggleEditing}
-              isActive={this.state.showForm}
-            />
+              <h1>Add one ya dummy.</h1>
+              <AddCard
+                handleClick={this.toggleEditing}
+                isActive={this.state.showForm}
+              />
             </span>
           </div>
-        }
-        
-        
+        )}
       </div>
     );
   }
