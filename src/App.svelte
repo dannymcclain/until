c9e9a3840ae3207383a4f3c9b99e1cf4ba224ff6
Danny McClain <dannyxmcclain@gmail.com>
2020-03-20 11:42:32 -0500

    recreating emoji comp

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Emoji from "./Components/Emoji.svelte";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello!</h1>
+  <p>
+    Checking to see if this
+    <Emoji label="paint brush" symbol="🖌" />
+    works or not.
+  </p>
+  <p>
+    What about this one?
+    <Emoji label="" symbol="🍎" />
+  </p>
+</main>
---
+++ src/Components/Emoji.svelte
@@ -0,0 +1,18 @@
+<script>
+  // *TOTALLY* based on https://www.npmjs.com/package/a11y-react-emoji by Sean McPherson.
+  export let symbol = "";
+  export let label = "";
+</script>
+
+<style>
+
+</style>
+
+<!-- I'm sure there's a more terse way of doing this, but this works just fine for me. -->
+{#if label}
+  <span class="emoji" role="img" aria-label={label} aria-hidden="false">
+    {symbol}
+  </span>
+{:else}
+  <span class="emoji" role="img" aria-hidden="true">{symbol}</span>
+{/if}
