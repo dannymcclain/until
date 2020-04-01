ba1a973dd5a8b62bc429efa907a15cee8276038d
Danny McClain <dannyxmcclain@gmail.com>
2020-04-01 18:23:11 -0500

    date pickers... am I right?

--- src/App.svelte
+++ src/App.svelte
@@ -38,6 +38,4 @@
     works or not.
   </p>
   <Event />
-
-  <Event />
 </main>
--- src/Components/Event.svelte
+++ src/Components/Event.svelte
@@ -1,22 +1,36 @@
 <script>
   import { format, formatDistanceToNow } from "date-fns";
+  import datepicker from "js-datepicker";
+  import { onMount } from "svelte";
+
+  let selection;
+  let pickerDate;
+  // let pickerInput;
+  onMount(() => {
+    // console.log("the component has mounted");
+    // pickerInput = document.getElementById("pickerInput");
+    // console.log(pickerInput);
+    const picker = datepicker("#pickerInput");
+  });
   // let selectedDate = format(new Date(), "yyyy, M, d");
-  let selectedDate = new Date().toDateString();
+  // let selectedDate = new Date().toDateString();
   // let selectedDate = "2020-5-2";
 
-  $: fromNow = formatDistanceToNow(new Date(selectedDate), {
-    addSuffix: false,
-    includeSeconds: true
-  });
+  // $: fromNow = formatDistanceToNow(new Date(selectedDate), {
+  //   addSuffix: false,
+  //   includeSeconds: true
+  // });
 </script>
 
 <style>
 
 </style>
 
-<input type="date" bind:value={selectedDate} />
+<!-- <input type="date" bind:value={selection} />
 <p>
   The selected date is
-  <strong>{selectedDate}</strong>
-</p>
-<p>It is in {fromNow}</p>
+  <strong>{selection}</strong>
+</p> -->
+<input id="pickerInput" type="text" bind:value={pickerDate} />
+<p>The pickerDate is {pickerDate}</p>
+<!-- <p>It is in {fromNow}</p> -->
