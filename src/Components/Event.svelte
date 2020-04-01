6f2d8e1daba67a3e03f27bba74a8cf4bf549259e
Danny McClain <dannyxmcclain@gmail.com>
2020-04-01 17:28:54 -0500

    Update Event.svelte

--- src/Components/Event.svelte
+++ src/Components/Event.svelte
@@ -1,6 +1,7 @@
 <script>
   import { format, formatDistanceToNow } from "date-fns";
-  let selectedDate = format(new Date(), "yyyy, M, d");
+  // let selectedDate = format(new Date(), "yyyy, M, d");
+  let selectedDate = new Date().toDateString();
   // let selectedDate = "2020-5-2";
 
   $: fromNow = formatDistanceToNow(new Date(selectedDate), {
