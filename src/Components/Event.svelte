f888a74646fff3ab23fa1844dd546be0fbe6c2f0
Danny McClain <dannyxmcclain@gmail.com>
2020-04-02 09:38:28 -0500

    Datepicker + moment working together

--- src/Components/Event.svelte
+++ src/Components/Event.svelte
@@ -1,36 +1,19 @@
 <script>
-  import { format, formatDistanceToNow } from "date-fns";
-  import datepicker from "js-datepicker";
-  import { onMount } from "svelte";
-
-  let selection;
-  let pickerDate;
-  // let pickerInput;
-  onMount(() => {
-    // console.log("the component has mounted");
-    // pickerInput = document.getElementById("pickerInput");
-    // console.log(pickerInput);
-    const picker = datepicker("#pickerInput");
-  });
-  // let selectedDate = format(new Date(), "yyyy, M, d");
-  // let selectedDate = new Date().toDateString();
-  // let selectedDate = "2020-5-2";
-
-  // $: fromNow = formatDistanceToNow(new Date(selectedDate), {
-  //   addSuffix: false,
-  //   includeSeconds: true
-  // });
+  import moment from "moment";
+  import Datepicker from "svelte-calendar";
+  let title = "Tomorrow";
+  let tomorrow = moment()
+    .add(1, "d")
+    .toDate();
+  let selectedDate = tomorrow;
+  $: fromNow = moment(selectedDate).fromNow();
 </script>
 
 <style>
 
 </style>
 
-<!-- <input type="date" bind:value={selection} />
-<p>
-  The selected date is
-  <strong>{selection}</strong>
-</p> -->
-<input id="pickerInput" type="text" bind:value={pickerDate} />
-<p>The pickerDate is {pickerDate}</p>
-<!-- <p>It is in {fromNow}</p> -->
+<input type="text" bind:value={title} />
+<Datepicker bind:selected={selectedDate} start={tomorrow} />
+
+<p>{title} is {fromNow}</p>
